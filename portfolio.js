3c4e7024350642459b0ed88c07ae20cee2c4d0e4
franchuf <franchuf@gmail.com>
2023-06-09 18:00:54 -0300

    mejorando la funcion

--- portfolio.js
+++ portfolio.js
@@ -1,22 +1,31 @@
 const fs = require ('fs')
 let inArray=[]
-let obj={}
+let obj=[]
 function getFile(){
   let info = JSON.parse(fs.readFileSync("./input.json",'utf-8'))
   inArray.push(info)
   return inArray
 }
-function outFile(){
+
+function arrayGroupedAvailability(){
   getFile()
-  let pesos=inArray[0].groupedAvailability[0].availability[0].amount
-  let dolarDivisa=inArray[0].groupedAvailability[1].availability[0].amount
-  let dolarBillete=inArray[0].groupedAvailability[2].availability[0].amount
-  obj={pesos:pesos,
-      dolarDivisa:dolarDivisa,
-      dolarBillete:dolarBillete}
-  return obj
-  
+  const array1=[]
+  inArray[0].groupedAvailability.forEach(element => {
+    array1.push(element.availability[0].amount)
+  }) 
+  return array1;
 }
-getFile()
-outFile()
-console.log(obj)
+
+
+// function outFile(){
+//   getFile()
+//   let pesos=inArray[0].groupedAvailability[0].availability[0].amount
+//   let dolarDivisa=inArray[0].groupedAvailability[1].availability[0].amount
+//   let dolarBillete=inArray[0].groupedAvailability[2].availability[0].amount
+//   obj={pesos:pesos,
+//       dolarDivisa:dolarDivisa,
+//       dolarBillete:dolarBillete}
+//   return obj
+  
+// }
+console.log(arrayGroupedAvailability())
